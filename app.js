403e1c83e74a22fce0dfb54d76e2591ba3aea85b
Gaurav Verma <gaurav.verma382001@gmail.com>
2023-07-23 11:34:35 +0530

    updated fixes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import dbConnect from "./config/database.mjs";
+import router from "./routes/api/apiRoutes.mjs";
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -9,35 +9,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-
-let database;
-async function MongoConnect() {
-    try{
-        const client =  await dbConnect();
-        database = client.db('todotest');
-        console.log('DB connected');
-    } catch(error) {
-        console.log(error);
-    }
-}
-MongoConnect();
-
-async function createTodoCollection(client) {
-    const database = client.db(process.env.DB_NAME); 
-    await database.createCollection('todos');
-    console.log('Todo collection created');
-}
-
-app.get('/', (req, res) => {});
-
-app.post('/', (req, res) => {});
-
-app.put('/:id', (req, res) => {});
-
-app.delete('/:id', (req, res) => {});
-
+app.use('/', router);
 
 app.listen(PORT);
