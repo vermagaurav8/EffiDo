a661a4b247c748854c07cef39dd8a991d66664f7
Gaurav Verma <gaurav.verma382001@gmail.com>
2023-07-23 12:20:48 +0530

    optimized functionlaity

--- controllers/apiController.mjs
+++ controllers/apiController.mjs
@@ -42,54 +42,81 @@ const getAllTodos = async (req, res) => {
     }
 }
 
-const getTodoById = async (req, res) => {
-    
-    const id = req.params.id;
-    if (!ObjectId.isValid(id)) {
-        return res.status(400).json({ error: 'Invalid Todo ID' });
-    }
+// Getting, updating, deleting todos by Id
 
+const validateTodoId = async (id, res) => {
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).json({ error: 'Invalid Todo ID' });
+    }
+  
     const todo = await database.collection(collectionName).findOne({ _id: new ObjectId(id) });
     if (!todo) {
-        return res.status(404).json({ error: 'Todo not found' });
+      return res.status(404).json({ error: 'Todo not found' });
     }
+  
+    return todo;
+};
 
-    res.status(201).json(todo);
+const getTodoById = async (req, res) => {
+    try{
+        const id = req.params.id;
+        const todo = await validateTodoId(id, res);
+        res.status(201).json(todo);
+    } catch(error) {
+        res.status(500).json(error);
+    }
+    
 }
 
 const updateTodoById = async (req, res) => {
-    const id = req.params.id;
-    if(!ObjectId.isValid(id)) {
-        return res.status(400).json({ error: 'Invalid Todo ID'});
+    try{
+        const id = req.params.id;
+        await validateTodoId(id, res);
+    
+        const update = req.body;
+        const updatedTodo = {...update, updatedAt: Date.now()}
+        await database.collection(collectionName).updateOne({ _id: new ObjectId(id) }, { $set: updatedTodo });
+        const updatedTodoFromDB = await database.collection(collectionName).findOne({ _id: new ObjectId(id) });
+        res.status(200).json(updatedTodoFromDB);
+    } catch(error){
+        res.status(500).json(error);
     }
-
-    const todo = await database.collection(collectionName).findOne({ _id: new ObjectId(id) });
-    if (!todo) {
-        return res.status(404).json({ error: 'Todo not found' });
-    }
-
-    const updatedTodo = req.body;
-    await database.collection(collectionName).updateOne({ _id: new ObjectId(id) }, { $set: updatedTodo });
-
-    const updatedTodoFromDB = await database.collection(collectionName).findOne({ _id: new ObjectId(id) });
-
-    res.status(200).json(updatedTodoFromDB);
 }
 
 const deleteTodoById = async (req, res) => {
-
+    try{
+        const id = req.params.id;
+        await validateTodoId(id, res);
+    
+        const result = await database.collection(collectionName).deleteOne({ _id: new ObjectId(id)});
+        if(result.deletedCount == 1){
+            res.status(200).json({ msg: "Todo Deleted Successfully" });
+        } else {
+            res.status(404).json({ msg: "Todo Not found" });
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
 }
 
-const markTodoCompleted = async (req, res) => {
+// const markTodoCompleted = async (req, res) => {
 
-}
+// }
 
 const getAllCompletedTodos = async (req, res) => {
+    try{
 
+    } catch(error) {
+        res.status(500).json(error)
+    }
 }
 
 const getAllPendingTodos = async (req, res) => {
+    try{
 
+    } catch(error) {
+        res.status(500).json(error);
+    }
 }
 
 export {
@@ -98,7 +125,6 @@ export {
     getTodoById,
     updateTodoById,
     deleteTodoById,
-    markTodoCompleted,
     getAllCompletedTodos,
     getAllPendingTodos
 }
--- routes/api/apiRoutes.mjs
+++ routes/api/apiRoutes.mjs
@@ -1,4 +1,4 @@
-import { addTodo, getAllTodos, getTodoById, updateTodoById, deleteTodoById, markTodoCompleted, getAllPendingTodos, getAllCompletedTodos } from "../../controllers/apiController.mjs";
+import { addTodo, getAllTodos, getTodoById, updateTodoById, deleteTodoById, getAllPendingTodos, getAllCompletedTodos } from "../../controllers/apiController.mjs";
 import express from "express";
 
 const router = express.Router();
@@ -10,7 +10,6 @@ router.get('/api/todos/:id', getTodoById);
 router.put('/api/todos/:id', updateTodoById);
 router.delete('/api/todos/:id', deleteTodoById);
 
-router.put('/api/todos/:id/completed', markTodoCompleted);
 
 router.get('/api/todos/completed', getAllCompletedTodos);
 router.get('/api/todos/pending', getAllPendingTodos);
